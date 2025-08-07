56eeae541cacb502b6a233bb59db8ade13ed933f
Fuad Rabbi <fuadhrabbi66@gmail.com>
2025-08-07 01:03:31 -0400

    Weather Tool

---
+++ src/main/java/com/dev/Rabbi/AiWeatherTool/AiWeatherToolApplication.java
@@ -0,0 +1,13 @@
+package com.dev.Rabbi.AiWeatherTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiWeatherToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiWeatherToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/Rabbi/AiWeatherTool/AiWeatherToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.Rabbi.AiWeatherTool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiWeatherToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
