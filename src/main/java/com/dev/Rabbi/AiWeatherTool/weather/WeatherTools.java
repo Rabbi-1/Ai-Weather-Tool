4d927c57c755239e6ed68c2cdff6c63963f3c1a8
Fuad Rabbi <fuadhrabbi66@gmail.com>
2026-04-08 18:35:57 -0400

    Add GeocodingService, enhance weather tools, and implement a mock weather UI

--- src/main/java/com/dev/Rabbi/AiWeatherTool/weather/WeatherTools.java
+++ src/main/java/com/dev/Rabbi/AiWeatherTool/weather/WeatherTools.java
@@ -11,7 +11,6 @@ import org.springframework.web.client.RestClientException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
